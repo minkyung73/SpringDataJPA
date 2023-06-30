46a81de5ee293ba0aea4477eff347d735318a4a6
minkyung <minkyung7365@gmail.com>
2023-06-30 14:39:36 +0900

    2-예제 도메인 모델과 동작 확인

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,38 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})   // team 이런 거 적으면 큰일남 (가급적이면 연관관계 필드는 ToString 안 하는 것이 좋다)
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")   // FK명
+    private Team team;
+
+    public Member(String username){
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null)
+            changeTeam(team);
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMemberList().add(this); // Team 도메인에 있는 memberList도 바꿔줘야 한다
+    }
+}
--- src/test/java/study/datajpa/DataJpaApplicationTests.java
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -1,7 +1,10 @@
 package study.datajpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberJpaRepository;
 
 @SpringBootTest
 class DataJpaApplicationTests {
